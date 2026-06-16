157e38448e0e584056dbd8d5fff9ff14d0cb40fe
Altyna1 <zoldosbekovaaltynaj30@gmail.com>
2026-06-16 19:34:15 +0600

    Improve login and registration UI

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
